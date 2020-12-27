326e9c3388c970b9bcd13540cb9086e1b65be96c
Danny Moulton <danny.moulton@kestros.io>
2020-12-27 13:04:22 -0500

    cleaned up dynamic tree leaf events

--- vendor-libraries/kestros-ui-foundation/src/main/resources/libs/kestros/vendor-libraries/kestros-ui-foundation/js/dynamic-tree/dynamic-tree-leaf.js
+++ vendor-libraries/kestros-ui-foundation/src/main/resources/libs/kestros/vendor-libraries/kestros-ui-foundation/js/dynamic-tree/dynamic-tree-leaf.js
@@ -1,25 +1,33 @@
 /*
-~      Copyright (C) 2020  Kestros, Inc.
-~
-~     This program is free software: you can redistribute it and/or modify
-~     it under the terms of the GNU General Public License as published by
-~     the Free Software Foundation, either version 3 of the License, or
-~     (at your option) any later version.
-~
-~     This program is distributed in the hope that it will be useful,
-~     but WITHOUT ANY WARRANTY; without even the implied warranty of
-~     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-~     GNU General Public License for more details.
-~
-~     You should have received a copy of the GNU General Public License
-~     along with this program.  If not, see <https://www.gnu.org/licenses/>.
-~
-*/
+    ~      Copyright (C) 2020  Kestros, Inc.
+    ~
+    ~     This program is free software: you can redistribute it and/or modify
+    ~     it under the terms of the GNU General Public License as published by
+    ~     the Free Software Foundation, either version 3 of the License, or
+    ~     (at your option) any later version.
+    ~
+    ~     This program is distributed in the hope that it will be useful,
+    ~     but WITHOUT ANY WARRANTY; without even the implied warranty of
+    ~     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    ~     GNU General Public License for more details.
+    ~
+    ~     You should have received a copy of the GNU General Public License
+    ~     along with this program.  If not, see <https://www.gnu.org/licenses/>.
+    ~
+    */
+
 
 /**
  * TreeLeaf with dynamically loaded child content.
  */
 class DynamicTreeLeaf extends TreeLeaf {
+
+  static get events() {
+    return {
+      LOAD: 'dynamic-tree-leaf-load'
+    };
+  }
+
   /**
    * DynamicContent element.
    *
@@ -43,7 +51,9 @@ class DynamicTreeLeaf extends TreeLeaf {
    */
   open() {
     super.open();
-    this.dynamicContentElement.dispatchEvent(new CustomEvent(DynamicContentArea.events.REFRESH));
+    for (let leaf of this.childLeaves) {
+      leaf.dispatchEvent(new CustomEvent(DynamicTreeLeaf.events.LOAD));
+    }
   }
 
   /**
@@ -51,5 +61,8 @@ class DynamicTreeLeaf extends TreeLeaf {
    */
   registerEventListeners() {
     super.registerEventListeners();
+    this.element.addEventListener(DynamicTreeLeaf.events.LOAD, () => {
+      this.dynamicContentElement.dispatchEvent(new CustomEvent(DynamicContentArea.events.REFRESH));
+    });
   }
 }
